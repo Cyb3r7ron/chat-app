8563e94c4a32ccd09c8a43e08801c21fe6a2247e
Kidus Telmid <45810961+Cyb3r7ron@users.noreply.github.com>
2023-06-25 08:16:42 -0400

    Update login.js

--- src/pages/login.js
+++ src/pages/login.js
@@ -88,7 +88,7 @@ const Login = () => {
             <div className="flex items-center flex-col">
                 <div className="text-center">
                     <div className="text-4xl font-bold">
-                        Login to Your Account
+                        እናውራ
                     </div>
                     <div className="mt-3 text-c3">
                         Connect and chat with anyone, anywhere
